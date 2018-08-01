00e1ba20d30d8417a7822c660b6317cffcee934f
Michael Caditz <michael@spaworks.com>
2018-08-01 13:44:51 -0700

    dfsgsdgh

--- Net-Zero/MainWindow.xaml.cs
+++ Net-Zero/MainWindow.xaml.cs
@@ -13,6 +13,13 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DevExpress.Xpf.Core;
 using DevExpress.Xpf.Charts;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Diagnostics;
+using Net_Zero.classes;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace Net_Zero
 {
@@ -61,6 +68,66 @@ namespace Net_Zero
            
 
 
+        }
+
+        private void SimpleButton_Click(object sender, RoutedEventArgs e)
+        {
+            int nProjectsID = 1001;
+            Nullable<DateTime> beginDate;
+            Nullable<DateTime> endDate;
+            beginDate = (DateEditStartDate.EditValue == null ? (Nullable<DateTime>)null : DBNull.Value.Equals(DateEditStartDate.EditValue) == true ? (Nullable<DateTime>)null :
+               (DateTime)DateEditStartDate.EditValue);
+            endDate = (DateEditEndDate.EditValue == null ? (Nullable<DateTime>)null : DBNull.Value.Equals(DateEditEndDate.EditValue) == true ? (Nullable<DateTime>)null :
+               (DateTime)DateEditEndDate.EditValue);
+
+            SqlConnection conn = new SqlConnection() { ConnectionString = ProgramSettings.net_zeroconnectionString };
+            try
+            {
+
+                using (SqlCommand cmd3 = new SqlCommand() { Connection = conn, CommandType = CommandType.StoredProcedure })
+                {
+                    //cmd3.Transaction = trans1;
+                    cmd3.Parameters.Clear();
+                    cmd3.CommandText = "dbo.createDates";
+                    cmd3.Parameters.AddWithValue("@projectsID", nProjectsID);
+                    cmd3.Parameters.AddWithValue("@beginDate", beginDate);
+                    cmd3.Parameters.AddWithValue("@endDate", endDate);
+
+                    //SqlParameter retval = cmd3.Parameters.Add("@transactIdentity", SqlDbType.Int);
+                    //retval.Direction = ParameterDirection.Output;
+                    conn.Open();
+                    cmd3.ExecuteNonQuery();
+                    //TransactID1 = (int)cmd3.Parameters["@transactIdentity"].Value;
+                }
+
+
+                Net_Zero.SummaryDataSet summaryDataSet = ((Net_Zero.SummaryDataSet)(this.FindResource("summaryDataSet")));
+                // TODO: Add code here to load data into the table getSummary.
+                // This code could not be generated, because the summaryDataSetgetSummaryTableAdapter.Fill method is missing, or has unrecognized parameters.
+
+                Net_Zero.SummaryDataSetTableAdapters.getSummaryTableAdapter summaryDataSetgetSummaryTableAdapter = new Net_Zero.SummaryDataSetTableAdapters.getSummaryTableAdapter();
+                summaryDataSetgetSummaryTableAdapter.Fill(summaryDataSet.getSummary, 1001);
+
+
+                System.Windows.Data.CollectionViewSource getSummaryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("getSummaryViewSource")));
+                getSummaryViewSource.View.MoveCurrentToFirst();
+
+            }
+
+
+            catch (Exception ex)
+            {
+                //utilities.errorLog(System.Reflection.MethodInfo.GetCurrentMethod().Name, ex);
+                System.ArgumentException argEx = new System.ArgumentException("New Line", "", ex);
+                throw argEx;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+
+                
+                //LocateNewLine(TransactID1);
+            }
         }
     }
 }
---
+++ Net-Zero/classes/ProgramSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_Zero.classes
+{
+    class ProgramSettings
+    {
+        static string database = "Net-Zero";
+        static string server = "Sager10";
+        // string server2 = Settings.Default.server2;
+        static string username = "sa";
+        static string password = "Sq!lsp!a123";
+
+
+        public static string net_zeroconnectionString =
+            String.Format("data source='{0}';initial catalog={1};password={2};persist security info=True;user id={3};packet size=4096;Connection Timeout=90", server, database, password, username);
+
+
+
+
+
+        // if (String.IsNullOrEmpty(server2)) server2 = server;
+
+        //Primary GW
+        // coolblueconnectionString = String.Format("data source='{0}';initial catalog={1};password={2};persist security info=True;user id={3};packet size=4096;Connection Timeout=90", server, database, password, username);
+
+    }
+}
