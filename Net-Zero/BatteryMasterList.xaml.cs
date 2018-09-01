f1a33a16c8b378928c5c2b65a92e476c5bdb0cd8
Michael Caditz <michael@spaworks.com>
2018-09-01 13:16:31 -0700

    xfc

--- Net-Zero/BatteryMasterList.xaml.cs
+++ Net-Zero/BatteryMasterList.xaml.cs
@@ -39,11 +39,106 @@ namespace Net_Zero
         {
 
             Net_Zero.Masters masters = ((Net_Zero.Masters)(this.FindResource("masters")));
-            // TODO: Add code here to load data into the table getBatteryMaster.
-            // This code could not be generated, because the mastersgetBatteryMasterTableAdapter.Fill method is missing, or has unrecognized parameters.
+           
+
             Net_Zero.MastersTableAdapters.getBatteryMasterTableAdapter mastersgetBatteryMasterTableAdapter = new Net_Zero.MastersTableAdapters.getBatteryMasterTableAdapter();
+            mastersgetBatteryMasterTableAdapter.Fill(masters.getBatteryMaster);
             System.Windows.Data.CollectionViewSource getBatteryMasterViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("getBatteryMasterViewSource")));
             getBatteryMasterViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void SimpleButtonNewBatteryMaster_Click(object sender, RoutedEventArgs e)
+        {
+            int TransactID1 = 0;
+            System.Windows.Data.CollectionViewSource getBatteryMasterViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("getBatteryMasterViewSource")));
+
+            Net_Zero.Masters masters = ((Net_Zero.Masters)(this.FindResource("masters")));
+
+
+            int nProjectsID = Settings.Default.nCurrentProjectID;
+            int stringCurrent = 0;
+
+            int wasnull = 0;
+            wasnull = (getBatteryMasterViewSource.View == null ? 1 : 0);
+            if (wasnull == 1)
+            {
+
+                // MessageBox.Show("Warning: uSP_getLineViewSource is null", "CoolBlue");
+                string message = "Warning: getBatteryMasterViewSource is null";
+                string caption = "Net-Zero";
+
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBoxResult defaultResult = MessageBoxResult.OK;
+                MessageBoxOptions options = MessageBoxOptions.RtlReading;
+                // Show message box
+                // MessageBoxResult result = MessageBox.Show(message, caption, buttons, icon, defaultResult, options);
+
+                // Displays the MessageBox.
+                MessageBoxResult result = MessageBox.Show(message, caption, buttons, icon, defaultResult, options);
+
+                if (result == MessageBoxResult.OK)
+                {
+
+                    // Closes the parent form.
+
+                    //this.Close();
+
+                }
+                return;
+            }
+            else
+            {
+                DataRowView drv = (DataRowView)getBatteryMasterViewSource.View.CurrentItem;
+                stringCurrent = (drv == null ? 0 : DBNull.Value.Equals(drv["nID"]) == true ? 0 : (int)drv["nID"]);
+            }
+
+
+
+
+
+
+            SqlConnection conn = new SqlConnection() { ConnectionString = ProgramSettings.net_zeroconnectionString };
+            try
+            {
+
+                using (SqlCommand cmd3 = new SqlCommand() { Connection = conn, CommandType = CommandType.StoredProcedure })
+                {
+                    //cmd3.Transaction = trans1;
+                    cmd3.Parameters.Clear();
+                    cmd3.CommandText = "[dbo].[USP_insertBatteryMaster]";
+
+                   // cmd3.Parameters.AddWithValue("@nBatterySeriesStringID", stringCurrent);
+                    // cmd3.Parameters.AddWithValue("@cName", "Series String");
+
+                    SqlParameter retval = cmd3.Parameters.Add("@transactIdentity", SqlDbType.Int);
+                    retval.Direction = ParameterDirection.Output;
+                    conn.Open();
+                    cmd3.ExecuteNonQuery();
+                    TransactID1 = (int)cmd3.Parameters["@transactIdentity"].Value;
+                }
+
+
+
+
+            }
+
+
+            catch (Exception ex)
+            {
+                //utilities.errorLog(System.Reflection.MethodInfo.GetCurrentMethod().Name, ex);
+                System.ArgumentException argEx = new System.ArgumentException("New Line", "", ex);
+                throw argEx;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+
+                //registerDataSet.EnforceConstraints = false;
+                Net_Zero.MastersTableAdapters.getBatteryMasterTableAdapter mastersgetBatteryMasterTableAdapter = new Net_Zero.MastersTableAdapters.getBatteryMasterTableAdapter();
+                mastersgetBatteryMasterTableAdapter.Fill(masters.getBatteryMaster);                //resetButtons();
+                //LocateNewLineBattery(TransactID1);
+            }
         }
     }
 }
--- Net-Zero/PVMasterList.xaml.cs
+++ Net-Zero/PVMasterList.xaml.cs
@@ -171,5 +171,195 @@ namespace Net_Zero
 
             }
         }
+
+        private void SimpleButtonSavePVMaster_Click(object sender, RoutedEventArgs e)
+        {
+            int nProjectsID = Settings.Default.nCurrentProjectID;
+
+            SavePVMaster();
+
+            
+
+            Net_Zero.Masters masters = ((Net_Zero.Masters)(this.FindResource("masters")));
+            Net_Zero.MastersTableAdapters.getPVMasterTableAdapter mastersgetPVMasterTableAdapter = new Net_Zero.MastersTableAdapters.getPVMasterTableAdapter();
+
+
+            mastersgetPVMasterTableAdapter.Fill(masters.getPVMaster);
+
+            System.Windows.Data.CollectionViewSource getPVMasterViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("getPVMasterViewSource")));
+
+
+
+        }
+        public void SavePVMaster()
+        {
+
+            System.Windows.Data.CollectionViewSource getPVMasterViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("getPVMasterViewSource")));
+
+            int nProjectsID = Settings.Default.nCurrentProjectID;
+
+            DataRowView drv = (DataRowView)getPVMasterViewSource.View.CurrentItem;
+            //int accountCurrent = (drv == null ? 0 : DBNull.Value.Equals(drv["ID"]) == true ? 0 : (int)drv["ID"]);
+
+            //int lineCurrent = 0;
+            int wasnull = 0;
+            wasnull = (getPVMasterViewSource.View == null ? 1 : 0);
+            if (wasnull == 1)
+            {
+
+                // MessageBox.Show("Warning: uSP_getLineViewSource is null", "CoolBlue");
+                string message = "Warning: getPVMasterViewSource is null";
+                string caption = "Net-Zero";
+
+                MessageBoxButton buttons = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBoxResult defaultResult = MessageBoxResult.OK;
+                MessageBoxOptions options = MessageBoxOptions.RtlReading;
+                // Show message box
+                // MessageBoxResult result = MessageBox.Show(message, caption, buttons, icon, defaultResult, options);
+
+                // Displays the MessageBox.
+                MessageBoxResult result = MessageBox.Show(message, caption, buttons, icon, defaultResult, options);
+
+                if (result == MessageBoxResult.OK)
+                {
+
+                    // Closes the parent form.
+
+                    //this.Close();
+
+                }
+                return;
+            }
+            else
+            {
+                DataRowView drv1 = (DataRowView)getPVMasterViewSource.View.CurrentItem;
+                //lineCurrent = (drv1 == null ? 0 : DBNull.Value.Equals(drv1["ID"]) == true ? 0 : (int)drv1["ID"]);
+            }
+
+
+
+
+            if (getPVMasterViewSource.View != null)
+
+            {
+                SqlConnection conn1 = new SqlConnection() { ConnectionString = ProgramSettings.net_zeroconnectionString };
+                getPVMasterViewSource.View.MoveCurrentToFirst();
+                for (int i = 0; i - 1 < i++; i++)
+                {
+
+                    DataRowView drv3 = (DataRowView)getPVMasterViewSource.View.CurrentItem;
+                    int nID = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nID"]) == true ? 0 : (int)drv3["nID"]);
+                    //MessageBox.Show(ID.ToString());
+
+                    if (nID == 0)
+                    {
+                        break;
+                    }
+
+                    decimal nPrice = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nPrice"]) == true ? 0 : (decimal)drv3["nPrice"]);
+                    // MessageBox.Show(nAmnt.ToString());
+
+
+
+                    string cURL = (DBNull.Value.Equals(drv3["cURL"]) == true ? "" : (string)drv3["cURL"]);
+                    string cVendor = (DBNull.Value.Equals(drv3["cVendor"]) == true ? "" : (string)drv3["cVendor"]);
+
+                    string cFrame = (DBNull.Value.Equals(drv3["cFrame"]) == true ? "" : (string)drv3["cFrame"]);
+                    string cBrand = (DBNull.Value.Equals(drv3["cBrand"]) == true ? "" : (string)drv3["cBrand"]);
+                    string cModel = (DBNull.Value.Equals(drv3["cModel"]) == true ? "" : (string)drv3["cModel"]);
+
+
+                    Boolean bDeleted = (drv3 == null ? false : DBNull.Value.Equals(drv3["bDeleted"]) == true ? false : (bool)drv3["bDeleted"]);
+
+
+                    decimal nPmax = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nPmax"]) == true ? 0 : (decimal)drv3["nPmax"]);
+                    decimal nVmp = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nVmp"]) == true ? 0 : (decimal)drv3["nVmp"]);
+                    decimal nImp = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nImp"]) == true ? 0 : (decimal)drv3["nImp"]);
+                    decimal nVoc = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nVoc"]) == true ? 0 : (decimal)drv3["nVoc"]);
+                    decimal nIsc = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nIsc"]) == true ? 0 : (decimal)drv3["nIsc"]);
+                    decimal nWeight_kg = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nWeight_kg"]) == true ? 0 : (decimal)drv3["nWeight_kg"]);
+                    decimal nLength_mm = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nLength_mm"]) == true ? 0 : (decimal)drv3["nLength_mm"]);
+                    decimal nHeight_mm = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nHeight_mm"]) == true ? 0 : (decimal)drv3["nHeight_mm"]);
+                    decimal nWidth_mm = (drv3 == null ? 0 : DBNull.Value.Equals(drv3["nWidth_mm"]) == true ? 0 : (decimal)drv3["nWidth_mm"]);
+
+                    /////write new record to dbo.split
+
+                    //SqlConnection conn1 = new SqlConnection() { ConnectionString = ProgramSettings.coolblueconnectionString };
+                    try
+                    {
+
+                        using (SqlCommand cmd3 = new SqlCommand() { Connection = conn1, CommandType = CommandType.StoredProcedure })
+                        {
+                            //cmd3.Transaction = trans1;
+                            cmd3.Parameters.Clear();
+                            cmd3.CommandText = "dbo.USP_updatePVMaster";
+                            cmd3.Parameters.AddWithValue("@nID", nID);
+
+                            cmd3.Parameters.AddWithValue("@cModel", cModel);
+                            cmd3.Parameters.AddWithValue("@cBrand", cBrand);
+                            cmd3.Parameters.AddWithValue("@nPmax", nPmax);
+                            cmd3.Parameters.AddWithValue("@nVmp", nVmp);
+                            cmd3.Parameters.AddWithValue("@nImp", nImp);
+                            cmd3.Parameters.AddWithValue("@nVoc", nVoc);
+                            cmd3.Parameters.AddWithValue("@nIsc", nIsc);
+                            cmd3.Parameters.AddWithValue("@nWeight_kg", nWeight_kg);
+                            cmd3.Parameters.AddWithValue("@nLength_mm", nLength_mm);
+                            cmd3.Parameters.AddWithValue("@nHeight_mm", nHeight_mm);
+                            cmd3.Parameters.AddWithValue("@nWidth_mm", nWidth_mm);
+                            cmd3.Parameters.AddWithValue("@cFrame", cFrame);
+                            cmd3.Parameters.AddWithValue("@cVendor", cVendor);
+                            cmd3.Parameters.AddWithValue("@nPrice", nPrice);
+
+                            cmd3.Parameters.AddWithValue("@cURL", cURL);
+
+                            cmd3.Parameters.AddWithValue("@bDeleted", bDeleted);
+
+
+
+                            //SqlParameter retval = cmd3.Parameters.Add("@transactIdentity", SqlDbType.Int);
+                            //retval.Direction = ParameterDirection.Output;
+                            conn1.Open();
+                            cmd3.ExecuteNonQuery();
+
+                        }
+
+
+
+
+                    }
+
+
+                    catch (Exception ex)
+                    {
+                        //utilities.errorLog(System.Reflection.MethodInfo.GetCurrentMethod().Name, ex);
+                        System.ArgumentException argEx = new System.ArgumentException("New Line", "", ex);
+                        throw argEx;
+                    }
+                    finally
+                    {
+                        if (conn1.State == ConnectionState.Open) conn1.Close();
+
+                        //registerDataSet.EnforceConstraints = false;
+
+                        //registerDataSetUSP_getSplitTableAdapter.Fill(registerDataSet.USP_getSplit, accountCurrent);
+                        //registerDataSetUSP_getLineTableAdapter.Fill(registerDataSet.USP_getLine, accountCurrent);
+                        //registerDataSet.EnforceConstraints = true;
+
+                        ////uSP_getLineDataGrid.
+
+                        ////uSP_getAllAccountTypesUSP_getAllAccountsViewSource.View.MoveCurrentToPosition(0);
+
+                        ////resetButtons();
+                        //LocateNewLine(TransactID1);
+                    }
+                    getPVMasterViewSource.View.MoveCurrentToNext();
+                }
+
+
+                
+            }
+
+        }
     }
 }
