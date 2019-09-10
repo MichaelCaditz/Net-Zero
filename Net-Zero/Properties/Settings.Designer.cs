888e41519813d5c9605d3901d15a02f3aef134a7
Michael Caditz Burrard <michael@spaworks.com>
2019-09-10 10:19:59 -0700

    TMY work

--- Net-Zero/Properties/Settings.Designer.cs
+++ Net-Zero/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Net_Zero.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -136,6 +136,16 @@ namespace Net_Zero.Properties {
             }
             set {
                 this["cUserFirst"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=BURRARD;Initial Catalog=Net-Zero;Integrated Security=True")]
+        public string Net_ZeroConnectionStringBurrard {
+            get {
+                return ((string)(this["Net_ZeroConnectionStringBurrard"]));
             }
         }
     }
